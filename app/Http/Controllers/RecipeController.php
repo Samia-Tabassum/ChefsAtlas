42e6e544d0dce78e8484d80644761745465ecf75
Samia-Tabassum <samiatabassum71@gmail.com>
2026-03-01 01:03:18 +0600

    post recipe

---
+++ app/Http/Controllers/RecipeController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Services\RecipeService;
+use Illuminate\Http\Request;
+
+class RecipeController extends Controller
+{
+    private RecipeService $recipeService;
+
+    public function __construct(RecipeService $recipeService)
+    {
+        $this->recipeService = $recipeService;
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+       return $this->recipeService->getRecipes();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        return response()->json([
+            'data' => [
+                'id' => $id,
+                'title' => 'Recipe ' . $id,
+                'description' => 'Description for recipe ' . $id,
+                'ingredients' => 'Ingredient list here',
+                'instructions' => 'Cooking instructions here',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $body = $request->json();
+
+        logger()->info('POST /recipes - Request body:', ['body' => $body]);
+        logger()->info('POST /recipes - All request data:', $request->all());
+
+        return response()->json([
+            'message' => 'Recipe created successfully',
+            'data' => [
+                'id' => rand(100, 999),
+                'title' => $body->get('title'),
+                'description' => $request->input('description', 'Recipe description'),
+                'ingredients' => $request->input('ingredients', 'Ingredients list'),
+                'instructions' => $request->input('instructions', 'Cooking instructions'),
+                'created_at' => now(),
+            ]
+        ], 201);
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,28 +1,13 @@
 <?php
 
-use App\Http\Controllers\UsersController;
+use App\Http\Controllers\RecipeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::get('recipes', [RecipeController::class, 'index']);
+Route::get('recipes/{id}', [RecipeController::class, 'show']);
 
-// Dummy CRUD operations for items using UsersController
-Route::get('/items', [UsersController::class, 'index']);
-Route::get('/items/{id}', [UsersController::class, 'show']);
-Route::post('/items', [UsersController::class, 'store']);
-Route::put('/items/{id}', [UsersController::class, 'update']);
-Route::patch('/items/{id}', [UsersController::class, 'patch']);
-Route::delete('/items/{id}', [UsersController::class, 'destroy']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('recipes', [RecipeController::class, 'store']);
+});
